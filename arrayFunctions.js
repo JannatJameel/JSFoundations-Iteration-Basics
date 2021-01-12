dc6d9f23e4e2b68b7434d301f807e97bcc48eb6d
JannatJameel <j_annat@outlook.com>
2021-01-12 10:00:26 +0300

    Functions done

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -8,8 +8,8 @@
  * getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]) -> [1, 3, 5, 7, 9]
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
-function getOdds(numbers) {
-  return numbers.filter(function (number) {
+function getOdds(numbers){
+  return numbers.filter(function (number){
     return number % 2 !== 0;
   });
 }
@@ -24,8 +24,8 @@ function getOdds(numbers) {
  * getEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]) -> [2, 4, 6, 8]
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
-function getEvens(numbers) {
-  return numbers.filter(function (number) {
+function getEvens(numbers){
+  return numbers.filter(function (number){
     return number % 2 === 0;
   });
 }
@@ -39,20 +39,13 @@ function getEvens(numbers) {
  * countOccurences(1, [1, 2, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 11, 1, 12, 13]) -> 4
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
-function countOccurences(x, numbers) {
+function countOccurences(x, numbers){
   let count = 0;
-  numbers.forEach(function (number) {
-    if (number === x) count++;
+  numbers.forEach(function (number){
+    if (number === x) count ++; 
   });
-  return count;
+  return count
 }
-
-// function countOccurences(x, numbers) {
-//   let numOccurances = numbers.filter(function (number) {
-//     return number === x;
-//   });
-//   return numOccurances.length;
-// }
 
 /**
  * makeThemDoctors(students):
@@ -62,11 +55,11 @@ function countOccurences(x, numbers) {
  * e.g.
  * makeThemDoctors(["Ali", "Aseel", "Richard"]) -> ["Dr. Ali", "Dr. Aseel", "Dr. Richard"]
  */
-
-function makeThemDoctors(students) {
-  return students.map(function (student) {
+function makeThemDoctors(students){
+  return students.map(function (student){
     return `Dr. ${student}`;
   });
 }
+
 
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
